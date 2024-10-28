cf72552f5c143febc7eb569b01398bdf0859561f
joudy003 <joudy.j.t@gmail.com>
2024-10-29 01:55:55 +0300

    Initial Commit

---
+++ mmmmm/mmmmmApp.swift
@@ -0,0 +1,17 @@
+//
+//  mmmmmApp.swift
+//  mmmmm
+//
+//  Created by Joud Abdullah AlAmri on 10/29/24.
+//
+
+import SwiftUI
+
+@main
+struct mmmmmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ mmmmmTests/mmmmmTests.swift
@@ -0,0 +1,17 @@
+//
+//  mmmmmTests.swift
+//  mmmmmTests
+//
+//  Created by Joud Abdullah AlAmri on 10/29/24.
+//
+
+import Testing
+@testable import mmmmm
+
+struct mmmmmTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
