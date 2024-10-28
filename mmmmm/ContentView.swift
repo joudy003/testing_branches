b89089dee03b0f503437582717a7eefd3d84a410
joudy003 <joudy.j.t@gmail.com>
2024-10-29 02:00:55 +0300

    88888

--- mmmmm/ContentView.swift
+++ mmmmm/ContentView.swift
@@ -3,9 +3,15 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
+         
+            Text("Hello, world!")
+            Text("Hello, world!")
+            Text("Hello, world!")
+            Text("Hello, world!")
+            Text("Hello, world!")
+            Text("Hello, world!")
+            Text("Hello, world!")
+            Text("Hello, world!")
             Text("Hello, world!")
         }
         .padding()
