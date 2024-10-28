5314b587ee16b9940fb60c364e8a67f474ef959f
joudy003 <joudy.j.t@gmail.com>
2024-10-29 02:02:30 +0300

    uuuuu

---
+++ mmmmm/SwiftUIView.swift
@@ -0,0 +1,19 @@
+import SwiftUI
+
+struct ContentView2: View {
+    var body: some View {
+        VStack {
+         
+            Text("Hello, world!")
+            Text("Hello, world!")
+            Text("Hello, world!")
+            
+            
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView2()
+}
